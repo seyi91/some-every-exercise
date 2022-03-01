c65c5af508fbdae822dda22cf5d2ca4310119c13
MADEYE <52187012+seyi91@users.noreply.github.com>
2022-03-01 14:50:10 -0600

    Add hasOnlyOddNumber()

--- some-every.js
+++ some-every.js
@@ -31,14 +31,19 @@ function hasAZero(num) {
 }
 
 /*
-Write a function called hasOnlyOddNumbers which accepts an array and returns true if every single number in the array is odd. If any of the values in the array are not odd, the function should return false. 
+Write a function called hasOnlyOddNumbers which accepts an array and returns true if every single number in the array is 
+odd. If any of the values in the array are not odd, the function should return false. 
 
 Examples:
     hasOnlyOddNumbers([1,3,5,7]) // true
     hasOnlyOddNumbers([1,2,3,5,7]) // false
 */
 
-function hasOnlyOddNumbers(arr) {}
+function hasOnlyOddNumbers(arr) {
+    return arr.some(function(val){
+        return val % 2 !== 0;
+    })
+}
 
 /*
 Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another). If there are any duplicates, the function should return false.
